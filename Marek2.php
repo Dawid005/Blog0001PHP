2db372fa80a62fd58666a85c874400b3e378a54d
Dawid005 <76645872+Dawid005@users.noreply.github.com>
2021-10-14 11:51:49 +0200

    fourth commit

--- Marek2.php
+++ Marek2.php
@@ -3,7 +3,5 @@
     require_once ('sqlQuery.php');
 
     $sql = "INSERT INTO marek_posts (content) VALUES ('$content')";
-    
-    sqlQuery($sql);
 
-?>
+    sqlQuery($sql);
--- Mareksee.php
+++ Mareksee.php
@@ -1,34 +1,7 @@
 <?php
 
-    require_once ('connectionMysql.php');
+    require_once ('sqlQuery.php')
 
-    try
-        {
-            $conn = new mysqli($connection->host, $connection->db_user, $connection->db_password, $connection->db_name);
-            if($conn -> connect_errno)
-            {   
-                throw new Exception(mysqli_connect_errno());
-            }   
+    $sql = "SELECT content FROM marek_posts";
 
-
-            $sql = "SELECT content FROM marek_posts";
-            $result = $conn->query($sql);
-            
-            if($result->num_rows >0){
-
-                while($row = $result->fetch_assoc()) {
-                    echo $row["content"]. "<br>";
-                  }
-            }
-
-            $conn->close();
-        }       
-        catch(Exception $e)
-        {       
-            echo '<span style="color:red;">Error of server! Sorry, please
-            make an account next time!</span>';
-            echo  '<br />Information for developers: '.$e;
-        }      
-
-
-?>
+    sqlQuery($sql);
